2e4a523c06517f73cbb9f8dae6391146f1d6f852
kludDesign <vincentgillibert@gmail.com>
2019-05-02 17:56:53 +0200

    Correct the city random in User controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,7 +9,8 @@ class UserController < ApplicationController
   end
 
   def create
-    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], description: params[:description], email: params[:email], age: params[:age], city_id: 62, password: params[:pass_first], password_confirmation: params[:pass_second]) # avec xxx qui sont les données obtenues à partir du formulaire
+    @city = City.all.sample.id
+    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], description: params[:description], email: params[:email], age: params[:age], city_id: @city, password: params[:pass_first], password_confirmation: params[:pass_second]) # avec xxx qui sont les données obtenues à partir du formulaire
 
     if @user.save # essaie de sauvegarder en base @gossip
       # si ça marche, il redirige vers la page d'index du site
